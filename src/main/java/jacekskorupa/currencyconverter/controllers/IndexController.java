2ab8041ed94371fb055a34bf66c7592119e02a6b
beginnerProgrammer3 <jaceksko91@gmail.com>
2020-08-21 22:06:15 +0200

    changed filetree

--- src/main/java/jacekskorupa/currencyconverter/controllers/IndexController.java
+++ src/main/java/jacekskorupa/currencyconverter/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package jacekskorupa.currencyconverter;
+package jacekskorupa.currencyconverter.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
