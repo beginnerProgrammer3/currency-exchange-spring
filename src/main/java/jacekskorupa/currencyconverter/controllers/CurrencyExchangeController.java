7f0a643ac46ea5ebfa779f9dba592d9be4c11195
beginnerProgrammer3 <jaceksko91@gmail.com>
2020-08-28 18:33:44 +0200

    CurrencyExchangeController refactoring

--- src/main/java/jacekskorupa/currencyconverter/controllers/CurrencyExchangeController.java
+++ src/main/java/jacekskorupa/currencyconverter/controllers/CurrencyExchangeController.java
@@ -18,71 +18,44 @@ import java.util.TreeMap;
 @RestController
 public class CurrencyExchangeController {
 
+    private final String API_KEY = "KFT0KZZ3V5FDR44G";
+
     @GetMapping(path = {"api/timeseriesweekly/{from}/and/{to}"})
     public List<CurrencyChangeToList> getWeeklyExchangeChanges(@PathVariable String from, @PathVariable String to) throws JsonProcessingException {
-        String weeklyCurrencyExchange = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=" + from + "&to_symbol=" + to + "&apikey=KFT0KZZ3V5FDR44G";
+        String weeklyCurrencyExchange = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=" + from + "&to_symbol=" + to + "&apikey=" + API_KEY;
 
-        RestTemplate restTemplate = new RestTemplate();
-        String exchangeRateString = restTemplate.getForObject(weeklyCurrencyExchange, String.class);
-
-        JSONObject obj = new JSONObject(exchangeRateString);
-        String pageName = obj.getJSONObject("Time Series FX (Weekly)").toString();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        TreeMap<String, CurrencyChange> currencesMap = objectMapper.readValue(pageName, objectMapper.getTypeFactory()
-                .constructMapLikeType(TreeMap.class, String.class,CurrencyChange.class));
-        List<CurrencyChangeToList> listToShow = new ArrayList<>();
-        currencesMap.forEach((String key, CurrencyChange currencyChange) -> {
-
-            CurrencyChangeToList currencyChangeToList = new CurrencyChangeToList(key, currencyChange.getOpen(), currencyChange.getHigh(), currencyChange.getLow(), currencyChange.getClose());
-            listToShow.add(currencyChangeToList);
-        });
-        return  listToShow;
+        return getListOfPeroidTimeCurrencyExchange(weeklyCurrencyExchange,"Weekly");
     }
 
     @GetMapping(path = {"api/timeseriesdaily/{from}/and/{to}"})
     public List<CurrencyChangeToList> getDailyExchangeChanges(@PathVariable String from, @PathVariable String to) throws JsonProcessingException {
-        String dailyCurrencyExchange = "https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=" + from + "&to_symbol=" + to + "&apikey=KFT0KZZ3V5FDR44G";
+        String dailyCurrencyExchange = "https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=" + from + "&to_symbol=" + to + "&apikey=" + API_KEY;
 
-        RestTemplate restTemplate = new RestTemplate();
-        String exchangeRateString = restTemplate.getForObject(dailyCurrencyExchange, String.class);
-
-        JSONObject obj = new JSONObject(exchangeRateString);
-        String pageName = obj.getJSONObject("Time Series FX (Daily)").toString();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        TreeMap<String, CurrencyChange> currencesMap = objectMapper.readValue(pageName, objectMapper.getTypeFactory()
-                .constructMapLikeType(TreeMap.class, String.class,CurrencyChange.class));
-
-        List<CurrencyChangeToList> listToShow = new ArrayList<>();
-        currencesMap.forEach((String key, CurrencyChange currencyChange) -> {
-
-            CurrencyChangeToList currencyChangeToList = new CurrencyChangeToList(key, currencyChange.getOpen(), currencyChange.getHigh(), currencyChange.getLow(), currencyChange.getClose());
-            listToShow.add(currencyChangeToList);
-        });
-        return  listToShow;
+        return getListOfPeroidTimeCurrencyExchange(dailyCurrencyExchange,"Daily");
     }
 
     @GetMapping(path = {"/apitimeseriesmonthly/{from}/and/{to}"})
     public List<CurrencyChangeToList> getMonthlyExchangeChanges(@PathVariable String from, @PathVariable String to) throws JsonProcessingException {
-        String monthlyCurrencyExchange = "https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol=" + from + "&to_symbol=" + to + "&apikey=KFT0KZZ3V5FDR44G";
+        String monthlyCurrencyExchange = "https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol=" + from + "&to_symbol=" + to + "&apikey=" + API_KEY;
 
-        RestTemplate restTemplate = new RestTemplate();
-        String exchangeRateString = restTemplate.getForObject(monthlyCurrencyExchange, String.class);
+        return getListOfPeroidTimeCurrencyExchange(monthlyCurrencyExchange,"Monthly");
+    }
 
-        JSONObject obj = new JSONObject(exchangeRateString);
-        String pageName = obj.getJSONObject("Time Series FX (Monthly)").toString();
+    public List<CurrencyChangeToList> getListOfPeroidTimeCurrencyExchange(String timeExchangeUrl, String peroid) throws JsonProcessingException {
+    RestTemplate restTemplate = new RestTemplate();
+    String exchangeRateString = restTemplate.getForObject(timeExchangeUrl, String.class);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        TreeMap<String, CurrencyChange> currencesMap = objectMapper.readValue(pageName, objectMapper.getTypeFactory()
-                .constructMapLikeType(TreeMap.class, String.class,CurrencyChange.class));
+    JSONObject obj = new JSONObject(exchangeRateString);
+    String pageName = obj.getJSONObject("Time Series FX ("+peroid+")").toString();
 
-        List<CurrencyChangeToList> listToShow = new ArrayList<>();
+    ObjectMapper objectMapper = new ObjectMapper();
+    TreeMap<String, CurrencyChange> currencesMap = objectMapper.readValue(pageName, objectMapper.getTypeFactory()
+            .constructMapLikeType(TreeMap.class, String.class,CurrencyChange.class));
+    List<CurrencyChangeToList> listToShow = new ArrayList<>();
         currencesMap.forEach((String key, CurrencyChange currencyChange) -> {
 
-            CurrencyChangeToList currencyChangeToList = new CurrencyChangeToList(key, currencyChange.getOpen(), currencyChange.getHigh(), currencyChange.getLow(), currencyChange.getClose());
-            listToShow.add(currencyChangeToList);
-        });
-        return  listToShow;
-    }
-}
+                CurrencyChangeToList currencyChangeToList = new CurrencyChangeToList(key, currencyChange.getOpen(), currencyChange.getHigh(), currencyChange.getLow(), currencyChange.getClose());
+                listToShow.add(currencyChangeToList);
+                });
+        return listToShow;
+    }}
