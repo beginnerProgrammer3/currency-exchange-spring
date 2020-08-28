f9adfd6216ff04b16544aaa29155fbbabc451a0b
beginnerProgrammer3 <jaceksko91@gmail.com>
2020-08-28 18:36:00 +0200

    CurrencyExchangeController refactoring

--- src/main/java/jacekskorupa/currencyconverter/controllers/CurrencyExchangeController.java
+++ src/main/java/jacekskorupa/currencyconverter/controllers/CurrencyExchangeController.java
@@ -20,21 +20,21 @@ public class CurrencyExchangeController {
 
     private final String API_KEY = "KFT0KZZ3V5FDR44G";
 
-    @GetMapping(path = {"api/timeseriesweekly/{from}/and/{to}"})
+    @GetMapping(path = {"/api/timeseriesweekly/{from}/and/{to}"})
     public List<CurrencyChangeToList> getWeeklyExchangeChanges(@PathVariable String from, @PathVariable String to) throws JsonProcessingException {
         String weeklyCurrencyExchange = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=" + from + "&to_symbol=" + to + "&apikey=" + API_KEY;
 
         return getListOfPeroidTimeCurrencyExchange(weeklyCurrencyExchange,"Weekly");
     }
 
-    @GetMapping(path = {"api/timeseriesdaily/{from}/and/{to}"})
+    @GetMapping(path = {"/api/timeseriesdaily/{from}/and/{to}"})
     public List<CurrencyChangeToList> getDailyExchangeChanges(@PathVariable String from, @PathVariable String to) throws JsonProcessingException {
         String dailyCurrencyExchange = "https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=" + from + "&to_symbol=" + to + "&apikey=" + API_KEY;
 
         return getListOfPeroidTimeCurrencyExchange(dailyCurrencyExchange,"Daily");
     }
 
-    @GetMapping(path = {"api/timeseriesmonthly/{from}/and/{to}"})
+    @GetMapping(path = {"/api/timeseriesmonthly/{from}/and/{to}"})
     public List<CurrencyChangeToList> getMonthlyExchangeChanges(@PathVariable String from, @PathVariable String to) throws JsonProcessingException {
         String monthlyCurrencyExchange = "https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol=" + from + "&to_symbol=" + to + "&apikey=" + API_KEY;
 
