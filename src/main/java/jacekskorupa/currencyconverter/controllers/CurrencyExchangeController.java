af436a021258cf72621b8d85653b63cc60048bcd
beginnerProgrammer3 <jaceksko91@gmail.com>
2020-09-06 14:56:42 +0200

    cors error

--- src/main/java/jacekskorupa/currencyconverter/controllers/CurrencyExchangeController.java
+++ src/main/java/jacekskorupa/currencyconverter/controllers/CurrencyExchangeController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.TreeMap;
 
 @RestController
-@CrossOrigin(origins = "https://currencyjsapp.herokuapp.com")
+@CrossOrigin(origins = "http://currencyjsapp.herokuapp.com")
 @RequestMapping({"/api"})
 public class CurrencyExchangeController {
 
--- src/main/java/jacekskorupa/currencyconverter/controllers/IndexController.java
+++ src/main/java/jacekskorupa/currencyconverter/controllers/IndexController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-@CrossOrigin(origins = "https://currencyjsapp.herokuapp.com")
+@CrossOrigin(origins = "http://currencyjsapp.herokuapp.com")
 @RestController
 @RequestMapping({"/api"})
 public class IndexController {
